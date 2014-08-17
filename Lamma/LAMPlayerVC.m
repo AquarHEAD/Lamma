c214755dab67ee6b9bd3efe19e67db1a164c5c3a
Xhacker Liu <liu.dongyuan@gmail.com>
2014-08-16 23:10:55 -0700

    Fix player.

--- Lamma/LAMPlayerVC.m
+++ Lamma/LAMPlayerVC.m
@@ -7,6 +7,7 @@
 //
 
 #import "LAMPlayerVC.h"
+#import "LAMPlayingShow.h"
 #import <AVFoundation/AVFoundation.h>
 
 @interface LAMPlayerVC ()
@@ -28,31 +29,37 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    self.show = [LAMPlayingShow sharedInstance].playingShow;
     self.title = self.show.title;
+
     // setup player
     self.playerItem = [AVPlayerItem playerItemWithURL:[NSURL URLWithString:self.show.audio]];
-//    [self.playerItem addObserver:self forKeyPath:@"status" options:0 context:&];
+    [self.playerItem addObserver:self forKeyPath:@"status" options:0 context:nil];
     self.player = [AVPlayer playerWithPlayerItem:self.playerItem];
     self.isPlaying = NO;
     self.currentTimeLabel.text = @"N/A";
     self.totalTimeLabel.text = @"N/A";
     self.playProgress.progress = 0.0f;
-//    [self.player addPeriodicTimeObserverForInterval:CMTimeMake(1, 2) queue:dispatch_get_main_queue() usingBlock:^(CMTime time) {
-//        if (self.player.status == AVPlayerStatusReadyToPlay) {
-//            NSUInteger currentSeconds = CMTimeGetSeconds([self.player currentTime]);
-//            NSUInteger minutes = floor(currentSeconds / 60);
-//            NSUInteger seconds = floor(currentSeconds % 60);
-//            self.currentTimeLabel.text = [NSString stringWithFormat:@"%lu:%lu", (unsigned long)minutes, (unsigned long)seconds];
-//        }
-//    }];
+    __weak __typeof__(self) weakSelf = self;
+    [self.player addPeriodicTimeObserverForInterval:CMTimeMake(1, 2) queue:dispatch_get_main_queue() usingBlock:^(CMTime time) {
+        if (weakSelf.player.status == AVPlayerStatusReadyToPlay) {
+            NSUInteger currentSeconds = CMTimeGetSeconds([weakSelf.player currentTime]);
+            NSUInteger minutes = floor(currentSeconds / 60);
+            NSUInteger seconds = floor(currentSeconds % 60);
+            weakSelf.currentTimeLabel.text = [NSString stringWithFormat:@"%lu:%lu", (unsigned long)minutes, (unsigned long)seconds];
+        }
+    }];
 
-//    NSUInteger totalSecond = CMTimeGetSeconds(self.player.currentItem.duration);
-//    NSUInteger minutes = floor(totalSecond / 60);
-//    NSUInteger seconds = floor(totalSecond % 60);
-//    self.totalTimeLabel.text = [NSString stringWithFormat:@"%lu:%lu", (unsigned long)minutes, (unsigned long)seconds];
+    NSUInteger totalSecond = CMTimeGetSeconds(self.player.currentItem.duration);
+    NSUInteger minutes = floor(totalSecond / 60);
+    NSUInteger seconds = floor(totalSecond % 60);
+    self.totalTimeLabel.text = [NSString stringWithFormat:@"%lu:%lu", (unsigned long)minutes, (unsigned long)seconds];
+
     // setup detailView
     self.detailView.backgroundColor = [UIColor clearColor];
     [self.detailView loadHTMLString:self.show.detail baseURL:nil];
+
     // setup button
     [self.controlButton setImage:[UIImage imageNamed:@"Pause"] forState:UIControlStateSelected];
 }
